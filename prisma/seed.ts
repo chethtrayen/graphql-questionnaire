8346a816db737a27321583442ebedda8a7a4ff46
chethtrayen <chethtrayen1991@gmail.com>
2022-12-01 16:11:06 -0500

    Add user seeding

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,7 +3,25 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 // eslint-disable-next-line
-async function main() {}
+async function main() {
+  await prisma.user.upsert({
+    where: { email: 'foo@bar.com'},
+    update: {},
+    create: {
+      email: 'foo@bar.com',
+      name: 'foobar'
+    }
+  });
+
+  await prisma.user.upsert({
+    where: { email: 'bar@foo.com'},
+    update: {},
+    create: {
+      email: 'bar@foo.com',
+      name: 'barfoo'
+    }
+  });
+}
 
 main()
   .catch((e) => {
--- src/types/index.ts
+++ src/types/index.ts
@@ -2,3 +2,4 @@ export * from "./auth";
 export * from "./config";
 export * from "./graphql";
 export * from "./request";
+export * from './user';
