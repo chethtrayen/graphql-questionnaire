3d62653764b538fe880422736fc88a0101e14e45
chethtrayen <chethtrayen1991@gmail.com>
2022-12-02 10:36:55 -0500

    Update test util resolver pathing

--- test/questionnaire/questionnaire.e2e-spec.ts
+++ test/questionnaire/questionnaire.e2e-spec.ts
@@ -3,14 +3,15 @@ import express from "express";
 import http from "http";
 import request from "supertest";
 
-import { getGraphQLContext } from '@context'
-import {resolvers, typeDefs} from '@testUtils'
+import { getGraphQLContext } from "@context";
+import { resolvers, typeDefs } from "@testUtils";
 
 describe("User e2e login", () => {
   let app: express.Application;
   let httpServer: http.Server;
   let server: ApolloServer;
-  const bearerTkn = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjY5OTQxMTI5fQ.84NoOMeWBhRFdx-qLIbZfImtNkLJAPoHPgJkisB_yxE";
+  const bearerTkn =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjY5OTQxMTI5fQ.84NoOMeWBhRFdx-qLIbZfImtNkLJAPoHPgJkisB_yxE";
 
   describe("POST /login", () => {
     const title = "foo@bar.com";
@@ -21,7 +22,7 @@ describe("User e2e login", () => {
       server = new ApolloServer({
         typeDefs,
         resolvers,
-        context: getGraphQLContext
+        context: getGraphQLContext,
       });
       await server.start();
       server.applyMiddleware({ app });
@@ -32,7 +33,6 @@ describe("User e2e login", () => {
     });
 
     it("should return a successful response", async () => {
-      
       const queryData = {
         query: `mutation create($title: String!) 
           {
@@ -40,21 +40,24 @@ describe("User e2e login", () => {
             {id, ownerId, status, title}
           }
           `,
-        variables: {title},
+        variables: { title },
       };
 
       const response = await request(httpServer)
         .post("/graphql")
-        .set('Authorization', 'Bearer ' + bearerTkn)
+        .set("Authorization", "Bearer " + bearerTkn)
         .send(queryData);
-      
+
       expect(response.statusCode).toBe(200);
-      expect(response.body.data.createQuestionnaire).toEqual(expect.objectContaining({
-        id: expect.any(Number),
-        ownerId: expect.any(Number),
-        status: expect.any(String),
-        title: expect.any(String),
-      }));
+      console.log(response.body.data);
+      expect(response.body.data.createQuestionnaire).toEqual(
+        expect.objectContaining({
+          id: expect.any(Number),
+          ownerId: expect.any(Number),
+          status: expect.any(String),
+          title: expect.any(String),
+        })
+      );
 
       expect(response.body.data.createQuestionnaire.title).toEqual(title);
     });
--- test/utils/resolvers.ts
+++ test/utils/resolvers.ts
@@ -1,6 +1,8 @@
-import {  mergeResolvers } from '@graphql-tools/merge'
-import { loadFilesSync } from '@graphql-tools/load-files'
+import { mergeResolvers } from "@graphql-tools/merge";
+import { loadFilesSync } from "@graphql-tools/load-files";
 import path from "path";
 
-const resolverArray = loadFilesSync(path.join(__dirname, '/../../src/app/resolvers/*.ts'))
-export const resolvers =  mergeResolvers(resolverArray);
+const resolverArray = loadFilesSync(
+  path.join(__dirname, "/../../src/app/modules/**/*.resolver.ts")
+);
+export const resolvers = mergeResolvers(resolverArray);
